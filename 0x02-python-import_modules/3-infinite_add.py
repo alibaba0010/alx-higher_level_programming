b8d0f70a45cc084e2be877136a50b82f0b4d1816
alibaba0010 <zakariyahali100@gmail.com>
2023-08-16 03:57:58 -0700

    Magic Calculator

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,8 @@
 
 if __name__ == "__main__":
     """Print the addition of all arguments."""
-    import sys
+    from sys import sys
     result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+    for arg in argv[1:]:
+        result += int(arg)
+    print("{:d}".format(result))
