1e149ada5557bfe213ec0aad62995a9df1602719
alibaba0010 <zakariyahali100@gmail.com>
2023-11-15 07:28:41 +0100

    task 3 debuggged

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
-      let s = "";
+      let s = '';
       for (let j = 0; j < this.width; j++) {
-        s += "X";
+        s += 'X';
       }
       console.log(s);
     }
