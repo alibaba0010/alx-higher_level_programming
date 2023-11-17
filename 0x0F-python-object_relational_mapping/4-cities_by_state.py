32faf9de040db312b84cab443cc60a1ec78d7952
alibaba0010 <zakariyahali100@gmail.com>
2023-11-17 11:41:52 +0100

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+                (argv[4], ))
+    query_rows = curs.fetchall()
+    for rows in query_rows:
+        print(rows)
+    curs.close()
+    conn.close()
+    
