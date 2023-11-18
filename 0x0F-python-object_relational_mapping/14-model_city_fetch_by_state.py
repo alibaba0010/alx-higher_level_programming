051ab6057b568d4d3065a55f67b6de6aefd4d052
alibaba0010 <zakariyahali100@gmail.com>
2023-11-18 16:58:17 +0100

    tasks done

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """List all states"""
 from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
+from model_state import Base, State
+from model_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
@@ -14,10 +14,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name="California")
-    session.add(new_state)
-    session.commit()
-    new_city = City(name="San Francisco", state_id=new_state.id)
-    session.add(new_city)
+    results = session.query(City, State).\
+        filter(City.state_id == State.id).all()
+    for city, state in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.commit()
     session.close()
